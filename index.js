411cdd01dff1da0a85fff930e2e4e2d779e17def
Grace Shih <vocalise@gmail.com>
2019-12-13 06:58:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, msg) {
+  const thank_you_array = [];
+  for (let i = 0; i < names.length; i++) {
+    thank_you_array.push(`Thank you, ${names[i]}, for the wonderful ${msg} gift!`);
+  }
+  return thank_you_array;
+
+}
+
+function countDown(num) {
+  for (let i = num; i >= 0; i--) {
+    console.log(i);
+  }
+}
